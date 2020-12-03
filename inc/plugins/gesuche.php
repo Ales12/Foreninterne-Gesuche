c71b4084105b7acf5a7c4a40c935fe1a289e75df
Alexandra <38242434+Ales12@users.noreply.github.com>
2020-12-03 12:50:29 +0100

    Update gesuche.php

--- inc/plugins/gesuche.php
+++ inc/plugins/gesuche.php
@@ -128,7 +128,7 @@ function gesuche_install()
 {$forenintern_menu}
 <td valign="top">
 <table width="100%">
-	<tr><td class="thead"><strong>{$lang->forenwanted}</h1>
+	<tr><td class="thead"><strong>{$lang->forenwanted}</strong>
 		</td></tr>
 <tr><td class="trow1"><div class="forenwanted_show">
 	{$forenwanted_show_bit_user}
